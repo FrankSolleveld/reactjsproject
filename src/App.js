fab9e130af4f4be402e140332db7123ab2951033
Frank Solleveld <fsolleveld@outlook.com>
2018-12-19 17:02:46 +0100

    Made GitHub cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
 import './App.css'
-// import { ReactComponent } from '*.svg';
+import axios from 'axios'
 
 // Should be presentational component
 const Card = (props) => {
@@ -24,4 +23,49 @@ const CardList = (props)=> {
   )
 }
 
-export default CardList
+class Form extends React.Component {
+  state = {userName: ''}
+  handleSubmit = (event) => {
+    // This will remember the last code you executed.
+    event.preventDefault();
+    console.log("Event: Form Submit", this.state.userName)
+    // ajax... (fetch or axios)
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    .then(resp => {
+      this.props.onSubmit(resp.data)
+      this.setState({userName: ''})
+    })
+  }
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+        value={this.state.userName}
+        onChange={(event) => this.setState({userName: event.target.value})}
+        placeholder="GitHub Username" required/>
+        <button type="submit">Add Card</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    cards: []
+  }
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }))
+  }
+  render(){
+    return(
+      <div>
+        <Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
+      </div>
+    )
+  }
+}
+
+export default <App />
